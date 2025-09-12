7c5f52d689740b90440020cf6963b7cd7845b18e
hanct249 <70663273+hanct249@users.noreply.github.com>
2025-09-12 13:34:40 -0600

    made basic linear model

--- BikeShare.R
+++ BikeShare.R
@@ -9,10 +9,13 @@ library(GGally)
 library(patchwork)
 
 bikeData <- vroom("trainBikeShare.csv")
+bikeData <- bikeData %>%
+  select(-c(casual,registered))
+testData <- vroom("testBikeShare.csv")
 dplyr::glimpse(bikeData)
 skimr::skim(bikeData)
 plot_intro(bikeData)
-plot_histogram()
+plot_histogram(bikeData)
 plot_correlation(bikeData)
 
 #Key features: temp, atemp, humidity, season, weather, windspeed
@@ -22,7 +25,6 @@ tempPlot <- ggplot(data=bikeData, mapping = aes(x=temp,y=count)) +
   geom_smooth(se=FALSE)
 weatherPlot <- ggplot(data=bikeData, mapping=aes(x=weather)) +
   geom_bar()
-weatherPlot
 humidPlot <- ggplot(data=bikeData, mapping = aes(x=humidity,y=count)) +
   geom_point() + 
   geom_smooth(se=FALSE)
@@ -30,3 +32,21 @@ windPlot <- ggplot(data=bikeData, mapping = aes(x=windspeed,y=count)) +
   geom_point() + 
   geom_smooth(se=FALSE)
 (tempPlot + weatherPlot) / (humidPlot + windPlot)
+
+my_linear_model <- linear_reg() %>%
+  set_engine("lm") %>%
+  set_mode("regression") %>%
+  fit(formula=count~.-datetime, data=bikeData)
+
+bike_predictions <- predict(my_linear_model, new_data=testData)
+bike_predictions
+
+kaggle_submission <- bike_predictions %>%
+  bind_cols(., testData) %>%
+  select(datetime, .pred) %>%
+  rename(count =.pred) %>%
+  mutate(count = pmax(0, count)) %>%
+  mutate(datetime = as.character(format(datetime)))
+
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
