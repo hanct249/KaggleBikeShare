c268bfe7a298b2b3ae8c91d9861d3930caf8dc35
hanct249 <70663273+hanct249@users.noreply.github.com>
2025-09-16 12:59:11 -0600

    added recipe

--- BikeShare.R
+++ BikeShare.R
@@ -10,7 +10,11 @@ library(patchwork)
 
 bikeData <- vroom("trainBikeShare.csv")
 bikeData <- bikeData %>%
-  select(-c(casual,registered))
+  select(-c(casual,registered)) %>%
+  mutate(count=log(count))
+
+str(bikeData$count)
+
 testData <- vroom("testBikeShare.csv")
 dplyr::glimpse(bikeData)
 skimr::skim(bikeData)
@@ -33,15 +37,31 @@ windPlot <- ggplot(data=bikeData, mapping = aes(x=windspeed,y=count)) +
   geom_smooth(se=FALSE)
 (tempPlot + weatherPlot) / (humidPlot + windPlot)
 
+bike_recipe <- recipe(count~., data= bikeData) %>%
+  step_mutate(weather= ifelse(weather == 4, 3, weather)) %>%
+  step_mutate(weather = factor(weather)) %>%
+  step_time(datetime,features=c("hour")) %>%
+  step_mutate(season = factor(season)) %>%
+  step_corr(all_numeric_predictors(), threshold = .1) %>%
+  step_rm(datetime) %>%
+  step_dummy(all_nominal_predictors())
+
+  
+
+
 my_linear_model <- linear_reg() %>%
   set_engine("lm") %>%
-  set_mode("regression") %>%
-  fit(formula=count~.-datetime, data=bikeData)
+  set_mode("regression")
 
-bike_predictions <- predict(my_linear_model, new_data=testData)
-bike_predictions
+bike_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(my_linear_model) %>%
+  fit(data=bikeData)
 
-kaggle_submission <- bike_predictions %>%
+lin_preds <- predict(bike_workflow, new_data = testData)
+
+
+kaggle_submission <- lin_preds %>%
   bind_cols(., testData) %>%
   select(datetime, .pred) %>%
   rename(count =.pred) %>%
