af52b4146a513a9bff1ac53da6961f03500d6eda
hanct249 <70663273+hanct249@users.noreply.github.com>
2025-09-24 13:02:18 -0600

    Tree predictions, forest predictions, bart predictions added

--- BikeShare.R
+++ BikeShare.R
@@ -7,6 +7,10 @@ library(skimr)
 library(DataExplorer)
 library(GGally)
 library(patchwork)
+library(rpart)
+library(ranger)
+
+#Set up training data
 
 bikeData <- vroom("trainBikeShare.csv")
 bikeData <- bikeData %>%
@@ -23,6 +27,7 @@ plot_histogram(bikeData)
 plot_correlation(bikeData)
 
 #Key features: temp, atemp, humidity, season, weather, windspeed
+# Initial graphs
 
 tempPlot <- ggplot(data=bikeData, mapping = aes(x=temp,y=count)) +
   geom_point() + 
@@ -37,12 +42,16 @@ windPlot <- ggplot(data=bikeData, mapping = aes(x=windspeed,y=count)) +
   geom_smooth(se=FALSE)
 (tempPlot + weatherPlot) / (humidPlot + windPlot)
 
+
+#Recipe for regression with penalties
+
+
 bike_recipe <- recipe(count~., data= bikeData) %>%
   step_mutate(weather= ifelse(weather == 4, 3, weather)) %>%
   step_mutate(weather = factor(weather)) %>%
   step_time(datetime,features=c("hour")) %>%
   step_mutate(season = factor(season)) %>%
-  #step_corr(all_numeric_predictors(), threshold = .5) %>%
+  step_corr(all_numeric_predictors(), threshold = .5) %>%
   step_rm(datetime) %>%
   step_dummy(all_nominal_predictors()) %>%
   step_normalize(all_numeric_predictors())
@@ -50,81 +59,113 @@ bike_recipe <- recipe(count~., data= bikeData) %>%
 prepped <- prep(bike_recipe)
 bake(prepped, new_data=bikeData)
 
-
-preg_model <- linear_reg(penalty=.1,mixture=0) %>%
-  set_engine("glmnet")
-preg_model2 <- linear_reg(penalty=.5,mixture=0) %>%
-  set_engine("glmnet")
-preg_model3 <- linear_reg(penalty=.1,mixture=1) %>%
-  set_engine("glmnet")
-preg_model4 <- linear_reg(penalty=.5,mixture=1) %>%
-  set_engine("glmnet")
-preg_model5 <- linear_reg(penalty=.1,mixture=0.5) %>%
+preg_model <- linear_reg(penalty=tune(),mixture=tune()) %>%
   set_engine("glmnet")
 
 preg_workflow <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(preg_model) %>%
+  add_model(preg_model)
+
+tuning_grid <- grid_regular(penalty(), mixture(), levels = 5)
+
+folds <- vfold_cv(bikeData, v = 5, repeats=1)
+
+CV_results <- preg_workflow %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(rmse,mae))
+
+collect_metrics(CV_results) %>%
+  filter(.metric=="rmse") %>%
+  ggplot(data=.,aes(x=penalty, y= mean, color=factor(mixture))) +
+  geom_line()
+
+bestTune <- CV_results %>%
+  select_best(metric="rmse")
+
+final_wf <- 
+  preg_workflow %>%
+  finalize_workflow(bestTune) %>%
   fit(data=bikeData)
-preg_workflow2 <- workflow() %>%
+
+preg_preds <- predict(final_wf, new_data = testData)
+
+
+#Regression tree
+
+
+reg_tree <- decision_tree(tree_depth = tune(),
+                          cost_complexity = tune(),
+                          min_n=tune()) %>%
+  set_engine("rpart") %>%
+  set_mode("regression")
+
+tree_workflow <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(preg_model2) %>%
+  add_model(reg_tree)
+
+tree_grid <- grid_regular(tree_depth(), cost_complexity(), min_n(), levels=5)
+
+folds <- vfold_cv(bikeData, v = 5, repeats=1)
+
+tree_results <- tree_workflow %>%
+  tune_grid(resamples = folds, grid=tree_grid, metrics=metric_set(rmse,mae))
+
+tree_bestTune <- tree_results %>%
+  select_best(metric="rmse")
+
+final_tree_wf <-
+  tree_workflow %>%
+  finalize_workflow(tree_bestTune) %>%
   fit(data=bikeData)
-preg_workflow3 <- workflow() %>%
+
+tree_preds <-predict(final_tree_wf, new_data = testData)
+
+
+# Random Forest
+
+
+forest_mod <- rand_forest(mtry = tune(),
+                          min_n=tune(),trees=500) %>%
+  set_engine("ranger") %>%
+  set_mode("regression")
+
+forest_wf <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(preg_model3) %>%
+  add_model(forest_mod)
+
+forest_grid <- grid_regular(mtry(range=c(1, 11)),
+                       min_n(),
+                       levels=5)
+
+for_folds <- vfold_cv(bikeData, v = 5, repeats = 1)
+
+forest_results <- forest_wf %>%
+  tune_grid(resamples= for_folds, grid= forest_grid, metrics=metric_set(rmse, mae))
+
+forest_bestTune <- forest_results %>%
+  select_best(metric = "rmse")
+
+final_forest_wf <- forest_wf %>%
+  finalize_workflow(forest_bestTune) %>%
   fit(data=bikeData)
-preg_workflow4 <- workflow() %>%
-  add_recipe(bike_recipe) %>%
-  add_model(preg_model4) %>%
-  fit(data=bikeData)
-preg_workflow5 <- workflow() %>%
-  add_recipe(bike_recipe) %>%
-  add_model(preg_model5) %>%
-  fit(data=bikeData)
 
-lin_preds <- predict(preg_workflow, new_data = testData)
-lin_preds2 <- predict(preg_workflow2, new_data = testData)
-lin_preds3 <- predict(preg_workflow3, new_data = testData)
-lin_preds4 <- predict(preg_workflow4, new_data = testData)
-lin_preds5 <- predict(preg_workflow5, new_data = testData)
+forest_preds <- predict(final_forest_wf, new_data=testData)
 
 
-kaggle_submission <- lin_preds %>%
+#Boosting
+
+
+
+
+kaggle_submission <- forest_preds %>%
   bind_cols(., testData) %>%
-  select(datetime, .pred) %>%
-  rename(count =.pred) %>%
-  mutate(count = pmax(0, count)) %>%
-  mutate(datetime = as.character(format(datetime)))
-kaggle_submission2 <- lin_preds2 %>%
-  bind_cols(., testData) %>%
-  select(datetime, .pred) %>%
-  rename(count =.pred) %>%
-  mutate(count = pmax(0, count)) %>%
-  mutate(datetime = as.character(format(datetime)))
-kaggle_submission3 <- lin_preds3 %>%
-  bind_cols(., testData) %>%
-  select(datetime, .pred) %>%
-  rename(count =.pred) %>%
-  mutate(count = pmax(0, count)) %>%
-  mutate(datetime = as.character(format(datetime)))
-kaggle_submission4 <- lin_preds4 %>%
-  bind_cols(., testData) %>%
-  select(datetime, .pred) %>%
-  rename(count =.pred) %>%
-  mutate(count = pmax(0, count)) %>%
-  mutate(datetime = as.character(format(datetime)))
-kaggle_submission5 <- lin_preds5 %>%
-  bind_cols(., testData) %>%
+  mutate(.pred = exp(.pred)) %>%
   select(datetime, .pred) %>%
   rename(count =.pred) %>%
   mutate(count = pmax(0, count)) %>%
   mutate(datetime = as.character(format(datetime)))
 
+vroom_write(x=kaggle_submission, file="./ForestPreds.csv", delim=",")
 
-vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
-vroom_write(x=kaggle_submission2, file="./LinearPreds2.csv", delim=",")
-vroom_write(x=kaggle_submission3, file="./LinearPreds3.csv", delim=",")
-vroom_write(x=kaggle_submission4, file="./LinearPreds4.csv", delim=",")
-vroom_write(x=kaggle_submission5, file="./LinearPreds5.csv", delim=",")
 
